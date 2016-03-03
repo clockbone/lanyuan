7a1e4214a3110ca8a1915a91472e892250baa569
sdfsad <sdfasdf>
2016-03-03 15:56:55 +0800

    初始化 mvn

---
+++ src/main/java/com/lanyuan/util/WebTool.java
@@ -0,0 +1,25 @@
+package com.lanyuan.util;
+
+/**
+ * Created by qinjun on 2016/2/2.
+ */
+public class WebTool {
+
+
+    public static PageIndex getPageIndex(long pagecode, int pageNow, long pageCount){
+        long startpage = pageNow-(pagecode%2==0? pagecode/2-1 : pagecode/2);
+        long endpage = pageNow+pagecode/2;
+        if(startpage<1){
+            startpage = 1;
+            if(pageCount>=pagecode) endpage = pagecode;
+            else endpage = pageCount;
+        }
+        if(endpage>pageCount){
+            endpage = pageCount;
+            if((endpage-pagecode)>0) startpage = endpage-pagecode+1;
+            else startpage = 1;
+        }
+        return new PageIndex(startpage, endpage);
+    }
+
+}
